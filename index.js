5010ed4bcc18bcc8cd2b8d44330ba9370c105ef1
Rodrigo Nikolof <rodrigo.nikolof@outlook.com>
2022-10-12 08:45:15 +1100

    commit tree 0deab972d2fbb7810d6cf8d38bf0548557d9948a

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+let num1 = 2;
+
+let num2 = 31;
+
+let num3 = 10;
+let num4 = 6;
+
+let num5 = (1, 15, 20);
+
+let multiply = num1 * num2;
+
+let random = parseInt(Math.random() * 100);
+
+let mod = num3 % num4;
+
+let max = Math.max(num5);
